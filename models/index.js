d231b17a5b90bd558403b13ec849b968046adbe5
bradleyjosephh <bradleyjosephh@gmail.com>
2022-01-02 17:12:03 -0800

    setting up server and model js

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./User.js')
+const Post = require('./Post.js')
+const Comments = require('./Note.js')
+
+
+User.hasMany(Post, { foreignKey: 'uid' })
+Post.belongsTo(User, { foreignKey: 'uid' })
+
+User.hasMany(Comments, { foreignKey: 'uid' })
+Comments.belongsTo(User, { foreignKey: 'uid' })
+
+Post.hasMany(Comments, { foreignKey: 'pid' })
+Comments.belongsTo(Post, { foreignKey: 'pid' })
+
+
+module.exports = { User, Post, Comments }
